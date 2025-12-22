69d5c7f59660f788d3445622446a4d2177a96247
Gregory Tolmachev <grtollm@gmail.com>
2025-12-23 02:56:49 +0300

    feat(sigma-algebra): add comaparision operator for outcome spaces

--- lib/sigma-algebra/OutcomeSpace.hpp
+++ lib/sigma-algebra/OutcomeSpace.hpp
@@ -17,6 +17,8 @@ public:
   [[nodiscard]] size_t GetSize() const noexcept;
   [[nodiscard]] const std::string& GetName(OutcomeId id) const;
 
+  bool operator==(const OutcomeSpace& other) const = default;
+
 private:
   std::vector<std::string> names_;
 };
